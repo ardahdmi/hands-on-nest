a8a51022338d0de082e300ddaaee987b59165903
Arda <ardahadimioglu@gmail.com>
2021-11-15 14:41:00 +0100

    adds Validation to provider

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -44,11 +44,18 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const foundTask = this.tasks.find((task) => task.id === id);
+
+    if (!foundTask) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+    return foundTask;
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const taskToDelete = this.getTaskById(id);
+
+    this.tasks = this.tasks.filter((task) => task.id !== taskToDelete.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
